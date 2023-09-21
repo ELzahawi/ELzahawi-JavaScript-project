f67b9c19a914db9551606d0f6cf51a6105d226ec
ELzahawi <139355785+ELzahawi@users.noreply.github.com>
2023-09-21 12:57:36 +0100

    new

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+window.alert("Hello,World");
+
+document.write("Hello,World!<br>");
+
+var A = "This is a string <br>"; //example on the variable
+document.write(A);
+
+var B = "This is my first Assignment on JS"
+window.alert(B);
+
+document.write("She said, \"I can't believe it's already Friday!\"<br>")
+
+document.write("\"The quick brown fox jumped over the lazy dog in the backyard,  "+"and the dog barked loudly in response.\"<br>")
+
+var C = "Pitman"+"Training!<br>";
+document.write(C)
+
+var car1 ="Mazda" ,car2 = "Toyota", car3 = "Kia"
+car4 = "Volvo";
+document.write("car3<br>");
+
+var num1 = 5, num2 = 7
+document.write(num1 + num2);
+
