46cb47401f179102fcc7400e8b2e07c8d8f8ab16
ELzahawi <139355785+ELzahawi@users.noreply.github.com>
2023-09-30 18:16:29 +0100

    New update2

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,56 @@
+document.write(typeof "HELLO");
+document.write("<br>")
+
+document.write(typeof 3);//printing the data type of the variable
+document.write("<br>")
+
+document.write("15" + 6);//an expression combining a string and a number
+document.write("<br>")
+
+function my_function(){
+document.getElementById("Test").innerHTML =- 3E310;
+}//
+
+function Assignment(){
+    document.getElementById("Mo").innerHTML = 2E310;
+}
+
+document.write(10 > 5);
+document.write("<br>")
+
+console.log(4 + 4);
+document.write("<br>")
+
+document.write(2 < 0);
+document.write("<br>")
+
+console.log(4 > 10);
+document.write("<br>")
+
+document.write(10 == 10);//== operator
+document.write("<br>")
+
+document.write(14 === 14);//===  operator
+document.write("<br>")
+
+document.write("Apple" === 14);
+document.write("<br>")
+
+document.write("14" === 14);
+document.write("<br>")
+
+document.write(10 === 14);
+document.write("<br>")
+
+document.write(5 < 10 && 3 < 15);//AND operator!
+document.write("<br>")
+
+document.write(5 < 10 || 3 < 15);//OR operator!
+document.write("<br>")
+
+function not_function(){
+    document.getElementById("Not").innerHTML = !(30 > 10);
+}//Not operator!
+
+
+
